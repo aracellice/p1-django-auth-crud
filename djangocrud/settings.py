e7ee90fd7d0a6573261907ba0addd760568ac889
Aracelli Centeno <aracellice@gmail.com>
2024-02-05 02:20:51 -0500

    update4

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
        # default='postgresql://postgres:postgres@localhost:5432/mysite',
-        default='postgresql://postgres:postgres@localhost/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
