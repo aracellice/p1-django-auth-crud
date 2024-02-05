c51bd88146d5097c6d78e2e48fadd4f8b63af32e
Aracelli Centeno <aracellice@gmail.com>
2024-02-05 02:29:39 -0500

    update5

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -145,3 +145,10 @@ LOGIN_URL = '/signin'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
